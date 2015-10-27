7eb22cd648cdfd72be11484cbfcc81cfd4305109
David Naranjo <dnaranjo89@gmail.com>
2015-10-27 10:33:50 +0100

    Add functionality to search and clean up JS code

--- src/js/map_wrapper.js
+++ src/js/map_wrapper.js
@@ -1,6 +1,37 @@
 
 var AccesibleMap = {};
 
+// Setup accesible marker
+AccesibleMap.accesible_icon = L.icon({
+        iconUrl: '/simplegeo/images/accesible_marker.png',
+        //shadowUrl: 'leaf-shadow.png',
+
+        iconSize:     [38, 38], // size of the icon
+        //shadowSize:   [50, 64], // size of the shadow
+        iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
+        //shadowAnchor: [4, 62],  // the same for the shadow
+        popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+    });
+
+AccesibleMap.search_markers = [];
+
+AccesibleMap.setup = function(){
+    var mapa = L.map('map', { zoomControl: false });
+    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+        attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a>',
+        maxZoom: 20,
+        id: 'dnaranjo89.97a21b99',
+        accessToken: 'pk.eyJ1IjoiZG5hcmFuam84OSIsImEiOiJjaWc1Z2p4ZTc0MW0ydWttM3Mxem44cmVlIn0.qYwIDUVfbIQ2x2a9IQgg-g'
+    }).addTo(mapa);
+    mapa.setView([39.472499, -6.376273], 15);
+    new L.Control.Zoom({ position: 'bottomright' }).addTo(mapa);
+    AccesibleMap.mapa = mapa;
+    $("#search-btn").click(function(){
+        var input_query = $("#search-box").val();
+        AccesibleMap.search(input_query);
+    });
+};
+
 AccesibleMap.test_route = function(){
     var origen = L.latLng(39.474346, -6.375368);
     var parking = L.latLng(39.473684, -6.377592);
@@ -14,16 +45,67 @@ AccesibleMap.draw_complete_route = function (origen, parking, destination, step_
     AccesibleMap.calculate_route_pedestrian(parking, destination, step_penalty).addTo(AccesibleMap.mapa);
 };
 
+AccesibleMap.search = function(query){
+    // Clean previous results
+    AccesibleMap.search_markers.map(function(marker){
+        AccesibleMap.mapa.removeLayer(marker);
+    });
 
-AccesibleMap.setup = function(){
-    var mapa = L.map('map', { zoomControl: false });
-    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-        attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a>',
-        maxZoom: 20,
-        id: 'dnaranjo89.97a21b99',
-        accessToken: 'pk.eyJ1IjoiZG5hcmFuam84OSIsImEiOiJjaWc1Z2p4ZTc0MW0ydWttM3Mxem44cmVlIn0.qYwIDUVfbIQ2x2a9IQgg-g'
-    }).addTo(mapa);
-    mapa.setView([39.472499, -6.376273], 15);
+    var num_results = 1;
+    var url = "https://search.mapzen.com/v1/search?api_key=search-AU6x3Ho&text=" + query + "&boundary.country=ES&size=" + num_results;
+    $.get(url).done(function(response){
+        response.features.map(function(location){
+            var cords = location.geometry.coordinates;
+            var pos =  [cords[1], cords[0]];
+            var title = location.properties.label;
+            var html_title = location.properties.label +
+                '<div class="text-center">' +
+                '<a href="#" class="btn btn-default btn-xs" onclick="AccesibleMap.add_destination(['+pos+']);">Ir Aqui</a>'+
+                '</>';
+            var marker = AccesibleMap.add_marker(pos, html_title);
+            marker.openPopup();
+            AccesibleMap.search_markers.push(marker);
+        });
+    }).fail(function(){
+        console.log("Couldn't find location: " + url);
+    });
+};
+
+AccesibleMap.add_destination = function(destination){
+    console.log("add destination: " +destination);
+};
+
+AccesibleMap.show_current_location = function (){
+    function onLocationFound(e) {
+        var radius = e.accuracy / 2;
+
+        L.marker(e.latlng).addTo(AccesibleMap.mapa)
+            .bindPopup("You are within " + radius + " meters from this point").openPopup();
+
+        L.circle(e.latlng, radius).addTo(AccesibleMap.mapa);
+    }
+    function onLocationError(e) {
+        alert(e.message);
+    }
+
+    AccesibleMap.mapa.on('locationfound', onLocationFound);
+    AccesibleMap.mapa.on('locationerror', onLocationError);
+    AccesibleMap.mapa.locate({setView: true, maxZoom: 16});
+};
+
+/**
+ * Internal functions
+ */
+
+AccesibleMap.add_marker = function(location, title, type){
+    var options = {
+        "title": title,
+    };
+    if (type == "parking"){
+        options['icon'] = AccesibleMap.accesible_icon;
+    }
+    return L.marker(location, options).addTo(AccesibleMap.mapa)
+        .bindPopup(title);
 };
 
 AccesibleMap.calculate_route_auto = function(origen, destination){
