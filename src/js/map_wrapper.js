fa12c04155c317807224020de1fb22511c01ac0e
David Naranjo <dnaranjo89@gmail.com>
2015-10-27 10:31:28 +0100

    Add JS to calculate routes

--- src/js/map_wrapper.js
+++ src/js/map_wrapper.js
@@ -11,3 +11,40 @@ AccesibleMap.setup = function(){
     }).addTo(mapa);
     mapa.setView([39.472499, -6.376273], 15);
 };
+
+AccesibleMap.calculate_route_auto = function(origen, destination){
+    var waypoints = [origen, destination];
+    var costing_options = {};
+    var mode = "auto";
+    return AccesibleMap.calculate_route(waypoints, mode, costing_options);
+};
+
+AccesibleMap.calculate_route_pedestrian = function(origen, destination, avoid_stairs){
+    var waypoints = [origen, destination];
+    var step_penalty = avoid_stairs ? 99999 : 0;
+    var costing_options = {"pedestrian": {"step_penalty": step_penalty }};
+    var mode = "pedestrian";
+    return AccesibleMap.calculate_route(waypoints, mode, costing_options);
+};
+
+AccesibleMap.calculate_route = function (waypoints, mode, costing_options){
+    var styles = [
+            {color: 'white',opacity: 0.8, weight: 12},
+            {color: '#2676C6', opacity: 1, weight: 6}
+        ];
+    styles[1].color = (mode == "pedestrian") ? '#76c626' : '#2676C6';
+
+    var options = {
+        parking_route: mode,
+        waypoints: waypoints,
+        lineOptions: {
+            styles: styles
+        },
+        router: L.Routing.valhalla('valhalla-dWJ_XBA', mode, costing_options),
+        formatter: new L.Routing.Valhalla.Formatter(),
+        summaryTemplate:'<div class="start">{name}</div><div class="info {transitmode}">{distance}, {time}</div>',
+        routeWhileDragging: false
+    };
+
+    return L.Routing.control(options);
+};
